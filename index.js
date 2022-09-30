ac20fc4810f3b01cd1e6c97d13eca85e05c2be9f
Toshiki Imai <tosiki_801_life@outlook.jp>
2022-09-30 12:11:07 +0900

    add debug message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function onSubmit(){
     const text = document.getElementById('answer').value;
-    console.log(text);
 
     const msg = "【送信内容】\n" + text;
+    console.log(msg);
     sendText(msg);
 }
