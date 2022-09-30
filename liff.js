0b0d5e90e425cb3eefd1d2276f0277ab4e7df718
toshiki imai <tosiki_801_life@outlook.jp>
2022-10-01 00:15:50 +0900

    liffを再生成

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1657513497-kbe1e8w5";
+    var liffId = "1657519737-xOkVknjY";
     console.log(`init liff, ID : ${liffId}`);
     initializeLiff(liffId);
 })
